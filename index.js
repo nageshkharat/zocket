169347abfabed8e8eb8d421cefe0ccada637191f
Nagesh Kharat <130240484+nageshkharat@users.noreply.github.com>
2024-04-25 15:07:50 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CanvasEditor from './CanvasEditor'; // Assuming the file is named CanvasEditor.js
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CanvasEditor />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
